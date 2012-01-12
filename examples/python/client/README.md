960ae4dce0fd36d7f22481be25bf9fd3ea696ba4
Andy <amastbaum@gmail.com>
2012-01-12 11:15:40 -0500

    multiple stream for python client; also update docs

--- examples/python/client/README.md
+++ examples/python/client/README.md
@@ -9,3 +9,7 @@ client.py
 
 The client listens for incoming event data (PackedRec objects), and reconstructs the original data.
 
+N.B. You can connect to multiple dispatcher streams at the same time. Add them like this:
+
+    cli.add_server('tcp://host:port')
+
--- lib/python/avalanche/avalanche.py
+++ lib/python/avalanche/avalanche.py
@@ -5,16 +5,23 @@ import ROOT
 class Client:
     '''a client subscribes to an avalanche server and receives ROOT TObjects'''
     def __init__(self, address):
-        self.address = address
+        self.serv_addr = []
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
         self.socket.setsockopt(zmq.SUBSCRIBE, '')
+        self.add_server(address)
+
+    def add_server(self, address):
+        '''connect to another stream. clients may listen to an unlimited number
+        of streams, the messages from which are interleaved.
+        '''
+        self.serv_addr.append(address)
         self.socket.connect(address)
 
     def recv_object(self, cls, flags=0):
         '''receives and returns a TObject of the class specified by TClass
         `cls`. recv_object blocks by default, but may be made non-blocking
-        with the zmq NOBLOCK flag
+        with the zmq NOBLOCK flag.
         '''
         try:
             msg = self.socket.recv(flags=flags, copy=False)
