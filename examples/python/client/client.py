3a07bb8ecdc6b6139bc73d209df18a8e1a05a27a
Andy <amastbaum@gmail.com>
2012-01-31 00:34:14 -0500

    update python example client with updated api

--- examples/python/client/client.py
+++ examples/python/client/client.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 import avalanche
+import signal
+import sys
 from rat import ROOT
 
+def signal_handler(signal, frame):
+    print 'Caught Ctrl-C, exiting...'
+    sys.exit(0)
+
 if __name__ == '__main__':
-    # create client listening to localhost port 5024
-    cli = avalanche.Client('tcp://localhost:5024')
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # create avalanche client
+    client = avalanche.Client()
+
+    # connect client to a dispatcher stream localhost port 5024
+    client.add_dispatcher('tcp://localhost:5024')
+
+    # connect client to a couchdb server at localhost:5984/dispatch
+    #client.add_db('http://localhost:5984', 'dispatch', filter=dispatch/headers)
 
     # receive RAT::DS::PackedRec objects
     while True:
-        rec = cli.recv_object(ROOT.RAT.DS.PackedRec.Class())
-
-        if rec:
+        rec = client.recv()
+        if rec is not None:
             print 'Received PackedRec of type', rec.RecordType
-        else:
-            print 'Error deserializing message data'
 
