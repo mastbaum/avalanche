161b3a57cfc592571b676fbe0a7e52fe4b660a0f
Andy <amastbaum@gmail.com>
2012-01-31 20:40:51 -0500

    cpp client api in progress. probably doesn't compile.

--- lib/cpp/avalanche.hpp
+++ lib/cpp/avalanche.hpp
@@ -1,12 +1,15 @@
-#include <string>
-#include <vector>
-#include <zmq.hpp>
-
 #ifndef __AVALANCHE_H__
 #define __AVALANCHE_H__
 
+#include <string>
+#include <vector>
+#include <queue>
+#include <pthread.h>
+#include <zmq.hpp>
+#include <json_value.h>
+
 class TObject;
-class TClass;
+class RAT::DS::PackedRec;
 
 namespace avalanche {
 
@@ -25,16 +28,38 @@ class server
 
 class client {
     public:
-        client(std::string _addr);
+        client();
         ~client();
-	void addServer(std::string _addr);
-	void* recvObject(TClass* cls, int flags=0);
+	void addDispatcher(std::string _addr);
+        void addDB(std::string _host, std::string _dbname, std::string _filterName, std::string _user, std::string _pass);
+        std::map<std::string, std::vector<std::string> > getStreamList();
+	TObject* recv(bool blocking=false);
 
     protected:
-        std::vector<std::string> serv_addr;
+        std::queue<TObject*> queue;
         zmq::context_t* context;
-        zmq::socket_t* socket;
+        std::vector<pthread_t*> threads;
+        std::map<std::string, std::vector<string> > streams;
 };
+
+struct dispatcherState {
+    std::queue<TObject*>* queue;
+    zmq::socket_t* socket;
+};
+
+struct dbState {
+    std::queue<TObject*>* queue;
+    std::string host;
+    std::string dbname;
+    std::string filterName;
+    std::string username;
+    std::string password;
+};
+
+void* watchDispatcher(void* arg);
+void* watchDB(void* arg);
+
+RAT::DS::PackedRec* docToRecord(Json::Value* doc);
 
 } // namespace avalanche
 
--- lib/cpp/client.cpp
+++ lib/cpp/client.cpp
@@ -9,51 +9,111 @@
 
 namespace avalanche {
 
-client::client(std::string _addr) {
-    // set up zeromq sockets
-    context = new zmq::context_t(1);
-    socket = new zmq::socket_t(*context, ZMQ_SUB);
-    socket->setsockopt(ZMQ_SUBSCRIBE, "", 0);
-    addServer(_addr);
-}
+client::client() { }
 
 client::~client() {
-    delete socket;
+    for (size_t i=0; i<threads.size(); i++) {
+        threads[i].join();
+        delete threads[i];
+    }
+
+    while (!queue.empty()) {
+        queue.pop();
+    }
+
     delete context;
 }
 
-void client::addServer(std::string _addr) {
-    serv_addr.push_back(_addr);
+void client::addDispatcher(std::string _addr) {
+    if (!socket) {
+        // set up zeromq sockets
+        context = new zmq::context_t(1);
+        socket = new zmq::socket_t(*context, ZMQ_SUB);
+        socket->setsockopt(ZMQ_SUBSCRIBE, "", 0);
+        streams["dispatcher"].append(_addr);
+
+        // launch dispatcher thread with (shared) state
+        dispatcherState s;
+        s.queue = &queue;
+        s.socket = socket;
+
+        pthread_t* dispatcherThread;
+        pthread_create(dispatcherThread, NULL, watchDispatcher, (void*) &s);
+        threads.push_back(dispatcherThread);
+    }
+
+    // add a connection to the socket
     socket->connect(_addr.c_str());
 }
 
-void* client::recvObject(TClass* cls, int flags) {
-    zmq::message_t message;
+void client::addDB(std::string _host, std::string _dbname, std::string _filterName, std::string _user, std::string _pass) {
+    streams["couchdb"].append(host + "/" + dbname);
 
-    try {
-        socket->recv(&message, flags);
-        if (message.size() == 0)
-            return NULL;
+    // launch couchdb-watching thread with given state
+    dbState s;
+    s.queue = &queue;
+    s.host = _host;
+    s.dbname = _dbname;
+    s.filterName = _filterName;
+    s.username = _user;
+    s.password = _pass;
 
-        // TBufferFile used for TObject serialization
-        TBufferFile bf(TBuffer::kRead, message.size(), message.data(), false);
-        void* bfo = bf.ReadObjectAny(cls);
+    pthread_t* couchThread;
+    pthread_create(couchThread, NULL, watchDB, (void*) &s);
+    threads.push_back(couchThread);
+}
 
-        // make a copy, since the buffer goes away
-        void* o = NULL;
-        if (bfo) {
-            o = cls->New();
-            memcpy(o, bfo, cls->Size());
-        }
+TObject* client::recv(bool blocking) {
+    while (blocking && queue.size() == 0)
+        continue;
 
+    if (queue.size() == 0) {
+        return NULL;
+    else {
+        TObject* o = new TObject(queue.front());
+        queue.pop();
         return o;
     }
-    catch (zmq::error_t e) {
-        if (e.num() == EAGAIN) // no data in buffer
-            return NULL;
-        else
-            throw e;
+}
+
+void* watchDispatcher(void* arg) {
+    dispatcherState s = *((dispatcherState*) arg);
+
+    while(1) {
+        zmq::message_t message;
+
+        try {
+            s.socket->recv(&message, s.flags);
+            if (message.size() == 0)
+                continue;
+
+            // TBufferFile used for TObject serialization
+            TBufferFile bf(TBuffer::kRead, message.size(), message.data(), false);
+            TObject* o = bf.ReadObjectAny(cls);
+
+            // make a copy, since the buffer goes away
+            //void* o = NULL;
+            //if (bfo) {
+            //    o = TObject::Class()->New();
+            //    memcpy(o, bfo, cls->Size());
+            //}
+
+            s.queue->push(o);
+        }
+        catch (zmq::error_t e) {
+            if (e.num() == EAGAIN) // no data in buffer
+                return NULL;
+            else
+                throw e;
+        }
     }
+
+    return NULL;
+}
+
+void* watchDB(void* arg) {
+    dbState s = *((dbState*) arg);
+    return NULL;
 }
 
 } // namespace avalanche
